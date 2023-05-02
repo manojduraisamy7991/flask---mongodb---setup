6d9d613d2d8662c67f8f0432aaa8ab2a5a407581
Manoj D <108085929+manojduraisamy7991@users.noreply.github.com>
2023-05-03 04:33:54 +0530

    monngo connection

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_pymongo import PyMongo
+
+app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb+srv://patib91326:nPzT8U7FCKZaHYfH@cluster0.bd8lxx7.mongodb.net/employees?retryWrites=true&w=majority"
+
+mongo = PyMongo(app)
+
+@app.route("/")
+def index():
+    mongo.db.users.insert_one({"name": "John"})
+    return "Document inserted."
